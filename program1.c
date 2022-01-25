2339e5b51b23519d61eacf74483a49826a96e82f
Ved-Bhonsle <Ved.Bhonsle@einfochips.com>
2022-01-25 18:49:30 +0530

    This is a program to calculat gross salary

---
+++ program1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+int b_salary;
+
+printf("Enter your salary:");
+scanf("%d",@b_salary);
+
+printf("Your gross salary is %f",(b_salary+(0.4*b_salary)+(0.2*b_salary)));
+}
